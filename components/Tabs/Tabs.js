dbf54259139b675a277dab030dae326d5525b92a
unknown <kenneth.austin93@gmail.com>
2019-06-21 13:43:55 -0400

    almost ready --debugging tab content not displaying

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -8,9 +8,8 @@ class TabLink {
     
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
     
-     <- Delete this comment block when you work on the if statement
     // Check to see if this.tabData is equal to 'all'
-    if (this.tabData === `all`){
+    if (this.tabData === 'all'){
       // If `all` is true, select all cards regardless of their data attribute values
       this.cards = document.querySelectorAll('.card');
     } else {
@@ -19,7 +18,7 @@ class TabLink {
     }
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-     this.cards = Array.from(this.cards).map(card=> new TabCard(card));
+     this.cards = Array.from(this.cards).map(card => new TabCard(card));
 
     // Add a click event that invokes this.selectTab
     this.tabElement.addEventListener('click',() => {this.selectTab()});
@@ -50,11 +49,11 @@ class TabLink {
 class TabCard {
   constructor(cardElement){
     // Assign this.cardElement to the cardElement DOM reference
-     this.cardElement;
+     this.cardElement = cardElement;
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
-     this.cardElement;
+     this.cardElement.style.display = 'flex';
   }
 
 }
@@ -68,4 +67,5 @@ class TabCard {
 - In your .forEach() method's callback function, return a new instance of TabLink and pass in each tab as a parameter
 
 */
-let tabs = document.querySelectorAll();
+let tabs = document.querySelectorAll('.tab');
+tabs.forEach(tab => new TabLink(tab));
